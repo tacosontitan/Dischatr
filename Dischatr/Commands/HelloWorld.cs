1778b638d9eb8b4711a4c1addccf8e4b7e940c48
Taco <65432314+tradinglounge@users.noreply.github.com>
2021-12-23 21:17:44 -0600

    Organized files, created command template, created hello world command.

---
+++ Dischatr/Commands/HelloWorld.cs
@@ -0,0 +1,8 @@
+﻿using Discord.WebSocket;
+
+namespace Dischatr {
+    public sealed class HelloWorldCommand : ChatbotCommand {
+        public HelloWorldCommand() : base("hello") { }
+        public override void Invoke(SocketMessage message, string[] parameters) => Reply(message, $"Hello {message.Author.Mention}!");
+    }
+}
---
+++ Dischatr/_templates/ChatbotCommandTemplate.cs
@@ -0,0 +1,28 @@
+﻿using Dischatr;
+using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$ {
+    public sealed class $safeitemname$Command : ChatbotCommand {
+
+        /// <summary>
+        /// Creates a new instance of the command for invocation using the specified key.
+        /// </summary>
+        public $safeitemname$Command() : base("key") { }
+        /// <summary>
+        /// This method is where the magic of the command happens.
+        /// </summary>
+        /// <param name="message">This is the original message, as received from Discord.</param>
+        /// <param name="parameters">This is an inferred collection of parameters received from the incoming message.</param>
+        public override void Invoke(SocketMessage message, string[] parameters) {
+            // TODO: Do some cool things here.
+            // TODO: Reply to messages with messages using `Reply`:
+            Reply(message, "Woah, pump the command brakes there chatbot; this command is unimplemented.");
+        }
+    }
+}
